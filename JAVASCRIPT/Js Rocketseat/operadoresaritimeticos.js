0b375fb1a03a5fd58a63065d8b8ae10059e0c80c
Jonnhy Alves <91919483+Johnny-Alves@users.noreply.github.com>
2024-03-16 23:24:17 -0300

    Add files via upload

---
+++ JAVASCRIPT/Js Rocketseat/operadoresaritimeticos.js
@@ -0,0 +1,11 @@
+//multiplicação
+//divisão
+//sbutração
+//soma
+
+//resto da divisãp
+//incremento
+//decremento
+//exponencial
+
+// nao | not is ! // ou or ||  // e or and && // então or then if se ? // : se nao //
